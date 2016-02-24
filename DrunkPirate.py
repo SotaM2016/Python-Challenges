c38502b4b9974015d35bd4d0d8156a1ee6185c17
SotaM2016 <200899@gardenschool.edu.my>
2016-02-24 10:31:06 +0800

    DrunkPirates Done

---
+++ DrunkPirate.py
@@ -0,0 +1,1 @@
+import turtle from random import randint  turns = int(input("Enter the amount of turns the pirate makes: ")) window = turtle.Screen() pirate = turtle.Turtle()  while turns > 0:     random = randint(0, 360)     pirate.right(random)     pirate.forward(100)     turns -= 1 
