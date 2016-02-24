cfecffb7298fe473a18e76ae3ef229afac3b53bb
SotaM2016 <200899@gardenschool.edu.my>
2016-02-24 09:56:26 +0800

    Fixed Spacings

--- VowelCount.py
+++ VowelCount.py
@@ -1,1 +1,7 @@
-word = input("Enter a word: ") vowels = ['a', 'e', 'i', 'o', 'u'] amount = 0 for letter in word:     if letter in vowels:         amount += 1 print("There are " + str(amount) + " vowels in that word")
+word = input("Enter a word: ")
+vowels = ['a', 'e', 'i', 'o', 'u']
+amount = 0
+for letter in word:
+  if letter in vowels:
+    amount += 1
+print("There are " + str(amount) + " vowels in that word")
