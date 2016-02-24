eb2a613dc980694879ddf19b439da6188fbffee7
SotaM2016 <200899@gardenschool.edu.my>
2016-02-24 10:16:11 +0800

    Fixed Spacings

--- BracketsChecker.py
+++ BracketsChecker.py
@@ -1,1 +1,12 @@
-text = input("Enter a text you want to check: ") inwards = 0 outwards = 0 for string in text:     if string == '(':         inwards += 1     elif string == ')':         outwards += 1 if inwards == outwards:     print("The amount of brackets are the same") else:     print("The amount of brackets are not the same")
+text = input("Enter a text you want to check: ")
+inwards = 0
+outwards = 0
+for string in text:
+  if string == '(':
+    inwards += 1
+    elif string == ')':
+      outwards += 1
+if inwards == outwards:
+  print("The amount of brackets are the same")
+else:
+  print("The amount of brackets are not the same")
