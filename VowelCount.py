0aa9712f79fdbfdff00b3be4302263f31545417a
SotaM2016 <200899@gardenschool.edu.my>
2016-02-24 09:49:12 +0800

    VowelCount Done

---
+++ VowelCount.py
@@ -0,0 +1,1 @@
+word = input("Enter a word: ") vowels = ['a', 'e', 'i', 'o', 'u'] amount = 0 for letter in word:     if letter in vowels:         amount += 1 print("There are " + str(amount) + " vowels in that word")
