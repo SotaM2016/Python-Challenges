468236f2264c05e32899f47f831e144b628103b9
SotaM2016 <200899@gardenschool.edu.my>
2016-02-25 10:56:17 +0800

    BottlesOfCola Done

---
+++ BottlesOfCola.py
@@ -0,0 +1,5 @@
+bottles = 99
+while bottles > 0:
+    print(str(bottles) + " bottles of cola on the wall.")
+    print(str(bottles) + " bottles of cola, take one down, pass it around.")
+    bottles -= 1
