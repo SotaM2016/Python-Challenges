332b178c7660c2e1b11d9fbac7cb0d884ee01444
SotaM2016 <200899@gardenschool.edu.my>
2016-02-25 10:52:30 +0800

    Fixed Spacings

--- EvilMathsTeacher.py
+++ EvilMathsTeacher.py
@@ -1,2 +1,13 @@
 #The first number had to be printed in manually
-twob = 0 oneb = 1 amount = 2 current = 0 print(0) print(1) while amount <= 100:     current = twob + oneb     print(current)     twob = oneb     oneb = current     amount += 1
+twob = 0
+oneb = 1
+amount = 2
+current = 0
+print(0)
+print(1)
+while amount <= 100:
+  current = twob + oneb
+  print(current)
+  twob = oneb
+  oneb = current
+  amount += 1
