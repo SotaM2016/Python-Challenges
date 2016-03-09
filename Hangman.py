80a4a861314d1bf370bfd8a24c27c534e99ed48c
SotaM2016 <200899@gardenschool.edu.my>
2016-03-09 10:32:17 +0800

    HangMan (Semi Done)

--- Hangman.py
+++ Hangman.py
@@ -10,36 +10,48 @@ def getWordText(letters, word):
             wordtext += "_ "
     return wordtext
 
+def check(event):
+    global lives
+    if lives == 0:
+        return
+    guess = tbox.get()
+    if len(guess) == 1:
+        if guess in gletters:
+            errorbox.configure(text="You have guessed that alphabet already!")
+        else:
+            gletters.append(guess)
+            if guess in word:
+                errorbox.configure(text="Great guess!")
+                current = getWordText(gletters, word)
+                guessed.configure(text=current)
+                if '_' not in current:
+                    errorbox.configure(text="You have guessed the word correctly!")
+            else:
+                errorbox.configure(text="Your alphabet was not in the word!")
+                lives -= 1
+    else:
+        errorbox.configure(text="Please input only one alphabet!")
+    tbox.delete(0, 'end')
+    llabel.configure(text="Lives left: " + str(lives))
+    if lives == 0:
+        errorbox.configure("Game Over man! Game over!")
+    return
+
 wlist = ['Hangman', 'Wood', 'Trees', 'Fish']
 word = random.choice(wlist)
+lives = 7
 window = tkinter.Tk()
 window.wm_title("Hangman Game")
 gletters = []
-guessed = tkinter.Label(window, text = getWordText(gletters, word))
+guessed = tkinter.Label(window, text=getWordText(gletters, word))
 guessed.pack()
-finish = False
-window.update_idletasks()
-window.update()
-while finish == False:
-    while True:
-        guess = input("Enter your guessed alphabet: ")
-        if len(guess) == 1:
-            if guess in gletters:
-                print("You have guessed that alphabet already!")
-            else:
-                gletters.append(guess)
-                if guess in word:
-                    print("Great guess!")
-                    current = getWordText(gletters, word)
-                    guessed.configure(text = current)
-                    window.update_idletasks()
-                    window.update()
-                    if '_' not in current:
-                        print("You have guessed the word correctly!")
-                        finish = True
-                        break
-                else:
-                    print("Your alphabet was not in the word!")
-                break
-        else:
-            print("Please input only one alphabet!")
+label1 = tkinter.Label(window, text="Enter your guess in the box bellow")
+label1.pack()
+tbox = tkinter.Entry(window)
+tbox.bind("<Return>", check)
+tbox.pack()
+errorbox = tkinter.Label(window, text="Error Messages come here")
+errorbox.pack()
+llabel = tkinter.Label(window, text="Lives Left: " + str(lives))
+llabel.pack()
+window.mainloop()
